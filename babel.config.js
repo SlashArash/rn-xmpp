6816eafd47c576266c209161bbf304825f825b4d
SlashArash <arash.kadkhodaei@gmail.com>
2019-03-30 11:23:02 +0430

    change configuration files to use typescript, prettier, eslint.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    "presets": [
+      "babel-preset-expo",
+    ],
+    "plugins": [
+      "@babel/plugin-transform-runtime",
+    ],
+    "env": {
+      "production": {
+        "plugins": ["transform-remove-console"]
+      },
+    }
   };
 };
