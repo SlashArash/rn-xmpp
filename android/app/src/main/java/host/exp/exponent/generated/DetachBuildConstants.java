9dc6110ba76f79e45974a86a972a9a845a834ccc
SlashArash <arash.kadkhodaei@gmail.com>
2019-03-30 10:19:20 +0430

    first try to use xmpp lib in react native

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expca3f281ee0c24cc2a6e4021089082ae8://172.20.10.7:19000";
+
+}
